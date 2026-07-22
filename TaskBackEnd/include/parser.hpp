53f4f279838969e5f319af7826cce9f53825b6e8
matias <matiassosagaar@gmail.com>
2026-07-22 17:52:07 +0000

    Removed jsonSchema library from the project, going to be included as an external lib

--- TaskBackEnd/include/parser.hpp
+++
@@ -1,7 +0,0 @@
-#include "message.hpp"
-#include <nlohmann/json.hpp>
-
-namespace Parser {
-    using json = nlohmann::json;
-    json parse_string(const std::string&);
-};
--- TaskBackEnd/src/parser.cpp
+++
@@ -1,4 +0,0 @@
-#include "parser.hpp"
-nlohmann::json Parser::parse_string(const std::string& str) {
-    return nlohmann::json::parse(str);
-}
